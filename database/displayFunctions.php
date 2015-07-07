e47c2593de516ab0fff538073ea8454204a2629b
JHorm <johanneshorm@gmail.com>
2015-07-08 00:38:30 +0300

    bug dix

--- database/displayFunctions.php
+++ database/displayFunctions.php
@@ -1,22 +1,18 @@
 <?php
-$id = $_POST['id'];
-exerciseInfo($id);
-function exerciseInfo($id){
+$id = (isset($_POST['id']) ? $_POST['id'] : null);
 	//connect to the database
-					require_once("connection.php");
-					$conn=database();
-					//Query the database
-					$info = "";
-					$resultSet = $conn->query("SELECT * FROM exercise WHERE exercise.id=".$id."");
-					if($resultSet->num_rows != 0){
-						while($rows = $resultSet->fetch_assoc()){
-							$info = $rows['name']+"|"+$rows['url']+"|"+$rows['description'];
-						}
-						echo $info;
-					}
-					else{
-						echo"ERROR";
-					}
-		    		$conn->close();
-}
+require_once("connection.php");
+$conn=database();				//Query the database
+$info = "";
+$resultSet = $conn->query("SELECT * FROM gymdiary.exercise WHERE id=".$id."");
+	if($resultSet->num_rows != 0){
+		while($rows = $resultSet->fetch_assoc()){
+			$info = $rows['name']+"|"+$rows['url']+"|"+$rows['description'];
+			}
+		echo $info;
+	}
+	else{
+		echo"ERROR";
+	}
+$conn->close();
 ?>
