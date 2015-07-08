cb32c9b2e54afe40155a14b9a35d11c5b0fdc22f
JHorm <johanneshorm@gmail.com>
2015-07-08 20:16:41 +0300

    connection try 6

--- database/displayFunctions.php
+++ database/displayFunctions.php
@@ -2,12 +2,12 @@
 $id = (isset($_POST['id']) ? $_POST['id'] : null);
 if($id != null){
 	require_once("connection.php");
-	$conn=database();				//Query the database
+	$conn = database();				//Query the database
 	$info = "";
-	$resultSet = $conn->query("SELECT * FROM gymdiary.exercise WHERE ID=".$id."");
+	$resultSet = $conn->query("SELECT * FROM exercise WHERE ID=".$id."");
 		if($resultSet->num_rows != 0){
 			while($rows = $resultSet->fetch_assoc()){
-				print($rows);
+				print_r($rows);
 				$name = $rows['name'];
 				$url = $rows['url'];
 				$desc = $rows['description'];
