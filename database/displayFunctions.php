dda2e183029f6071c55f9746d2f65825b8c79dc0
JHorm <johanneshorm@gmail.com>
2015-07-08 20:09:59 +0300

    connection try 3

--- database/displayFunctions.php
+++ database/displayFunctions.php
@@ -7,7 +7,10 @@ if($id != null){
 	$resultSet = $conn->query("SELECT * FROM gymdiary.exercise WHERE ID=".$id."");
 		if($resultSet->num_rows != 0){
 			while($rows = $resultSet->fetch_assoc()){
-				print_r($rows['name']+"|"+$rows['url']+"|"+$rows['description']+" "+$id);
+				$name = $rows['name'];
+				$url = $rows['url'];
+				$desc = $rows['description'];
+				print_r($name+"|"$url+"|"+$desc);
 				}
 		}
 
