179d1e67d251cea27c8e80969756d9ae9014e145
JHorm <johanneshorm@gmail.com>
2015-07-08 17:48:31 +0300

    connection should work

--- database/displayFunctions.php
+++ database/displayFunctions.php
@@ -1,18 +1,20 @@
 <?php
 $id = (isset($_POST['id']) ? $_POST['id'] : null);
-	//connect to the database
-require_once("connection.php");
-$conn=database();				//Query the database
-$info = "";
-$resultSet = $conn->query("SELECT * FROM gymdiary.exercise WHERE id=".$id."");
-	if($resultSet->num_rows != 0){
-		while($rows = $resultSet->fetch_assoc()){
-			$info = $rows['name']+"|"+$rows['url']+"|"+$rows['description'];
-			}
-		echo $info;
-	}
-	else{
-		echo"ERROR";
-	}
-$conn->close();
+if($id != null){
+	require_once("connection.php");
+	$conn=database();				//Query the database
+	$info = "";
+	$resultSet = $conn->query("SELECT * FROM gymdiary.exercise WHERE ID=".$id."");
+		if($resultSet->num_rows != 0){
+			while($rows = $resultSet->fetch_assoc()){
+				$info = $rows['name']+"|"+$rows['url']+"|"+$rows['description'];
+				}
+			echo $info;
+		}
+
+		else{
+			echo"ERROR";
+		}
+	$conn->close();
+}
 ?>
